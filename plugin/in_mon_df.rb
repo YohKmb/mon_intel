294b015688854b45d2ff0ba5425af77940b108af
Yoh Kamibayashi <yoh134shonan@gmail.com>
2015-08-14 08:59:33 +0900

    first commit

---
+++ plugin/in_mon_df.rb
@@ -0,0 +1,60 @@
+module Fluent
+
+  class SomeOutput < Output
+    Fluent::Plugin.register_output("mon_df", self)
+
+    config_param :host, :string, :default => nil
+    config_param :port, :integer, :default => 80
+    config_param :base_uri, :string, :default => nil
+    config_param :ssl, :bool, :default => nil
+    config_param :user, :string, :default => nil
+    config_param :password, :string, :default => nil
+
+    def initialize
+      require "eapier"
+    end
+
+    def configure(conf)
+      super
+
+      unless @host
+        raise ConfigError, "'host' parameter is required on file output"
+      end
+      unless @user
+        raise ConfigError, "'user' parameter is required on file output"
+      end
+      unless @password
+        raise ConfigError, "'password' parameter is required on file output"
+      end
+
+      scheme = @ssl == true ? "https" : "http"
+      @base_uri = "#{scheme}://#{@host}:#{@port}/"
+    end
+
+    def start
+      super
+    end
+
+    def shutdown
+      super
+    end
+
+    # This method is called when an event reaches Fluentd.
+    # 'es' is a Fluent::EventStream object that includes multiple events.
+    # You can use 'es.each {|time,record| ... }' to retrieve events.
+    # 'chain' is an object that manages transactions. Call 'chain.next' at
+    # appropriate points and rollback if it raises an exception.
+    #
+    # NOTE! This method is called by Fluentd's main thread so you should not write slow routine here. It causes Fluentd's performance degression.
+    def emit(tag, es, chain)
+      chain.next
+      es.each {|time,record|
+        eapier = Eapier.new(["show version"], nil, "eapi", "password", nil,
+                            nil, nil, true, nil, true, "192.168.1.250")
+        $stderr.puts eapier.start
+        # $stderr.puts "OK. I got a log message !"
+      }
+    end
+
+  end
+end
